d23436596e29d1de55f41a2d69b4c2b1f22f5965
ramkitech1 <ramkitech1@gmail.com>
2026-03-05 11:39:15 +0530

    final

--- ticket_classifier.py
+++ ticket_classifier.py
@@ -28,12 +28,12 @@ X = df["cleaned_message"]
 y = df["category"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.2, random_state=35
 )
 
 
 nb_pipeline = Pipeline([
-    ("tfidf", TfidfVectorizer()),
+    ("tfidf", TfidfVectorizer(ngram_range=(1,2), min_df=2,max_df=0.9)),
     ("model", MultinomialNB())
 ])
 
@@ -49,9 +49,8 @@ print(classification_report(y_test, nb_preds))
 
 
 lr_pipeline = Pipeline([
-    ("tfidf", TfidfVectorizer()),
-    ("model", LogisticRegression(max_iter=1000))
-])
+    ("tfidf", TfidfVectorizer(ngram_range=(1,2), min_df=2,max_df=0.9)),
+    ("model", LogisticRegression(max_iter=1000,class_weight="balanced"))])
 
 lr_pipeline.fit(X_train, y_train)
 lr_preds = lr_pipeline.predict(X_test)
